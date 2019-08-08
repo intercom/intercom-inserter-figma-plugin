ffe78048d1c6c23e5fd46f4af71fbb6dc6f59ec9
Andrey Sundiev <asundiev@gmail.com>
2019-08-08 11:46:47 +0100

    Added analytics

--- src/code.ts
+++ src/code.ts
@@ -1,5 +1,14 @@
 import * as Components from './figmaComponents';
 
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+  return new (P || (P = Promise))(function (resolve, reject) {
+      function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+      function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+      function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+      step((generator = generator.apply(thisArg, _arguments || [])).next());
+  });
+};
+
 figma.showUI(__html__, { height: 500, width: 400 });
 
 figma.ui.onmessage = async ({ component, props }) => {
@@ -16,5 +25,8 @@ figma.ui.onmessage = async ({ component, props }) => {
 
   Components[component](props);
 
-  figma.closePlugin();
+  figma.ui.postMessage({ type: 'analytics-event', event: 'completed' });
+  figma.ui.onmessage = (msg) => __awaiter(this, void 0, void 0, function* () {
+      figma.closePlugin();
+  });
 };
